258e96d51274ac806776bd106e1b0424249918ef
Subhi-c <csubhiksha23@gmail.com>
2024-03-13 22:47:44 -0700

    Smallest letter greater than target

---
+++ Leetcode/SmallestLetterGreaterThanTarget.java
@@ -0,0 +1,22 @@
+package Leetcode;
+public class SmallestLetterGreaterThanTarget {
+ public static void main(String[] args) {
+    char[] letters={'a','b','d'};
+    char target = 'c';
+    System.out.println("The Letter Greater than target is: "+FindLetter(letters, target));
+ }  
+ public static char FindLetter(char[] letters, char target){
+        int start =0;
+        int end= letters.length -1;
+        int mid;
+        while(start <= end){
+            mid =(start +end)/2;
+            if(letters[mid] > target){
+                end = mid-1;
+            }else {
+                start = mid+1;
+            }
+        }
+        return letters[start % letters.length]; 
+ } 
+}
