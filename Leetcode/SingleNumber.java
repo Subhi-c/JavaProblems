a5c7cfc5a19ebbe149c1d6f84a70e24a9a3411c4
Subhi-c <csubhiksha23@gmail.com>
2024-03-15 10:41:02 -0700

    Single Number

---
+++ Leetcode/SingleNumber.java
@@ -0,0 +1,22 @@
+package Leetcode;
+
+import java.util.Arrays;
+
+class SingleNumber{
+    public static void main(String[] args) {
+        int[] arr ={1};
+        System.out.println(SingleNumberInAnArray(arr));
+    }
+    public static int SingleNumberInAnArray(int[] arr){
+        Arrays.sort(arr);
+        if(arr.length == 1){
+            return arr[0];
+        }
+        for(int i=0;i<arr.length-2;i=i+2){
+            if(arr[i]!=arr[i+1]){
+                return arr[i];
+            }
+        }
+        return arr[arr.length-1];
+    }
+}
