0eff89f118b41743324702e1b8ef8cd050a05f9d
Subhi-c <csubhiksha23@gmail.com>
2024-05-04 20:06:29 -0700

    update Largest Positive number that exists

--- Leetcode/LargestPositiveNumberThatExists.java
+++ Leetcode/LargestPositiveNumberThatExists.java
@@ -7,12 +7,13 @@ public class LargestPositiveNumberThatExists{
     }
     public static int FindExists(int[] arr){
         HashSet<Integer> set = new HashSet<Integer>();
+        int result = -1;
         for(int i=0;i<arr.length;i++){
             if(set.contains(-1*arr[i])){
-                return arr[i];
+                result = Math.max(result , Math.abs(arr[i]));
             }
             set.add(arr[i]);
         }
-        return 0;
+        return result;
     }
 }
